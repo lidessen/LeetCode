ab7f2bebb1948dbbc17e1091b58312077d83a394
ztytotoro <ztytotoro@outlook.com>
2019-03-01 16:42:48 +0800

    Update add-two-numbers.py

--- python/src/add-two-numbers.py
+++ python/src/add-two-numbers.py
@@ -20,14 +20,3 @@ class Solution:
                 l2 = l2.next
             start = self.add(start, l1, l2)
         return result
-    
-    def add(self, pre, l1, l2):
-        val = l1.val + l2.val + pre.val
-        if(val <= 9):
-            pre.val = val
-            if(l1.next is not None or l2.next is not None):
-                pre.next = ListNode(0)
-        else:
-            pre.val = val - 10
-            pre.next = ListNode(1)
-        return pre.next
