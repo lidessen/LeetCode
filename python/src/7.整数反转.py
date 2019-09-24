440c58aa214b636461f7e0c58df7d9715f93631a
Yarn <ztytotoro@outlook.com>
2019-09-24 23:55:21 +0800

    update

--- python/src/7.整数反转.py
+++ python/src/7.整数反转.py
@@ -3,7 +3,50 @@
 #
 # [7] 整数反转
 #
+# https://leetcode-cn.com/problems/reverse-integer/description/
+#
+# algorithms
+# Easy (32.49%)
+# Likes:    1345
+# Dislikes: 0
+# Total Accepted:    190.8K
+# Total Submissions: 577.3K
+# Testcase Example:  '123'
+#
+# 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+#
+# 示例 1:
+#
+# 输入: 123
+# 输出: 321
+#
+#
+# 示例 2:
+#
+# 输入: -123
+# 输出: -321
+#
+#
+# 示例 3:
+#
+# 输入: 120
+# 输出: 21
+#
+#
+# 注意:
+#
+# 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+#
+#
 class Solution:
     def reverse(self, x: int) -> int:
-        
-
+        result = 0
+        negative = -1 if x < 0 else 1
+        abs_x = abs(x)
+        while abs_x != 0:
+            _x = abs_x % 10
+            result = result * 10 + _x
+            abs_x = (abs_x - _x) / 10
+        result *= -1 if x < 0 else 1
+        return int(result) if result <= (pow(2, 31) -
+                                         1) and result >= pow(-2, 31) else 0
--- python/src/add-two-numbers.py
+++ python/src/add-two-numbers.py
@@ -4,20 +4,21 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
     def addTwoNumbers(self, l1, l2):
         result = ListNode(0)
-        cursor=result
-        carry=0
-        while(l1 or l2):
-            x= l1.val if l1 else 0
-            y= l2.val if l2 else 0
-            val=carry+x+y
-            carry=val//10
-            cursor.next=ListNode(val%10)
-            cursor=cursor.next
-            if(l1!=None):l1=l1.next
-            if(l2!=None):l2=l2.next
-        if(carry>0):
-            cursor.next=ListNode(1)
+        cursor = result
+        carry = 0
+        while (l1 or l2):
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            val = carry + x + y
+            carry = val // 10
+            cursor.next = ListNode(val % 10)
+            cursor = cursor.next
+            if (l1 != None): l1 = l1.next
+            if (l2 != None): l2 = l2.next
+        if (carry > 0):
+            cursor.next = ListNode(1)
         return result.next
