bb2c66c7c8bd6f2843eac0f716b533bb6b597a7d
Yarn <ztytotoro@users.noreply.github.com>
2020-04-10 11:42:31 +0800

    [919]完全二叉树插入器

---
+++ js/[树]919.完全二叉树插入器.js
@@ -0,0 +1,134 @@
+/*
+ * @lc app=leetcode.cn id=919 lang=javascript
+ *
+ * [919] 完全二叉树插入器
+ *
+ * https://leetcode-cn.com/problems/complete-binary-tree-inserter/description/
+ *
+ * algorithms
+ * Medium (59.16%)
+ * Likes:    12
+ * Dislikes: 0
+ * Total Accepted:    1.8K
+ * Total Submissions: 3K
+ * Testcase Example:  '["CBTInserter","insert","get_root"]\n[[[1]],[2],[]]'
+ *
+ * 完全二叉树是每一层（除最后一层外）都是完全填充（即，结点数达到最大）的，并且所有的结点都尽可能地集中在左侧。
+ *
+ * 设计一个用完全二叉树初始化的数据结构 CBTInserter，它支持以下几种操作：
+ *
+ *
+ * CBTInserter(TreeNode root) 使用头结点为 root 的给定树初始化该数据结构；
+ * CBTInserter.insert(int v) 将 TreeNode 插入到存在值为 node.val = v
+ * 的树中以使其保持完全二叉树的状态，并返回插入的 TreeNode 的父结点的值；
+ * CBTInserter.get_root() 将返回树的头结点。
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：inputs = ["CBTInserter","insert","get_root"], inputs = [[[1]],[2],[]]
+ * 输出：[null,1,[1,2]]
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：inputs = ["CBTInserter","insert","insert","get_root"], inputs =
+ * [[[1,2,3,4,5,6]],[7],[8],[]]
+ * 输出：[null,3,4,[1,2,3,4,5,6,7,8]]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 最初给定的树是完全二叉树，且包含 1 到 1000 个结点。
+ * 每个测试用例最多调用 CBTInserter.insert  操作 10000 次。
+ * 给定结点或插入结点的每个值都在 0 到 5000 之间。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var CBTInserter = function (root) {
+  this.rootNode = root;
+  const [count, level] = traverseTree(root);
+  this.count = count;
+  this.currentLevel = level - 1;
+  if (this.count === Math.pow(2, this.currentLevel + 1) - 1) {
+    this.currentLevel++;
+  }
+};
+
+function traverseTree(node) {
+  if (node === null) {
+    return [0, 0];
+  }
+  const [lCount, lLevel] = traverseTree(node.left);
+  const [rCount, rLevel] = traverseTree(node.right);
+  return [1 + lCount + rCount, Math.max(1 + lLevel, 1 + rLevel)];
+}
+
+/**
+ * @param {number} v
+ * @return {number}
+ */
+CBTInserter.prototype.insert = function (v) {
+  this.count++;
+
+  let index = this.count - Math.pow(2, this.currentLevel) + 1;
+  let levelCount = Math.pow(2, this.currentLevel);
+  let cursor = this.rootNode;
+  if (this.count === Math.pow(2, this.currentLevel + 1) - 1) {
+    this.currentLevel++;
+  }
+
+  while (levelCount > 1) {
+    levelCount = levelCount / 2;
+    if (levelCount === 1) {
+      if (index === 1) {
+        cursor.left = new TreeNode(v);
+      } else {
+        cursor.right = new TreeNode(v);
+      }
+
+      return cursor.val;
+    }
+    if (index <= levelCount) {
+      cursor = cursor.left;
+    } else {
+      cursor = cursor.right;
+      index = index - levelCount;
+    }
+  }
+};
+
+/**
+ * @return {TreeNode}
+ */
+CBTInserter.prototype.get_root = function () {
+  return this.rootNode;
+};
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * var obj = new CBTInserter(root)
+ * var param_1 = obj.insert(v)
+ * var param_2 = obj.get_root()
+ */
+// @lc code=end
