b50844c568e70e36e9c693071b3c9087a9bd91cd
Yarn <ztytotoro@users.noreply.github.com>
2020-05-19 22:50:37 +0800

    [53]最大子序和

---
+++ js/[动规]53.最大子序和.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=javascript
+ *
+ * [53] 最大子序和
+ *
+ * https://leetcode-cn.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (49.95%)
+ * Likes:    1966
+ * Dislikes: 0
+ * Total Accepted:    234.8K
+ * Total Submissions: 459.9K
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 示例:
+ *
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ *
+ *
+ * 进阶:
+ *
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let max = nums[0];
+  let lastMax = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    let maxI = Math.max(nums[i], lastMax + nums[i]);
+    if (maxI > max) {
+      max = maxI;
+    }
+    lastMax = maxI;
+  }
+  return max;
+};
+// @lc code=end
